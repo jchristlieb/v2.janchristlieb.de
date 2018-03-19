4600917a04f50fb5dc15e017d611fa6472205238
Jan <mail@janchristlieb.de>
2018-03-19 18:42:29 +0100

    refactoring

---
+++ database/migrations/2018_03_19_142528_create_projects_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->increments('id')->unique();
+            $table->year('date');
+            $table->string('client');
+            $table->string('repository');
+            $table->string('link');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('description');
+            $table->integer('image_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
