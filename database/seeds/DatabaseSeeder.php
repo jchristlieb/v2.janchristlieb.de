26c0604558ac2a1196091bcfa4b529369020df17
Jan <mail@janchristlieb.de>
2018-03-19 18:40:26 +0100

    defined seeding for tags and projects

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        // create projects
+        $projects = factory(\App\Project::class, 7)->create();
+        $this->command->info('Projects created');
+
+        // create tags
+        $tags = collect();
+        collect(['PHP', 'JavaScript', 'JQuery', 'WordPress', 'CSS', 'Laravel'])->each(function ($tag) use ($tags) {
+           $tags->push(factory(\App\Tag::class)->create([
+               'name' => $tag,
+               'slug' => str_slug($tag),
+           ]));
+        });
+        $this->command->info('Tags created');
+
+        // seed project_tag relationship table
+
+        // each project has many tags (rand 1 to 4)
     }
 }
