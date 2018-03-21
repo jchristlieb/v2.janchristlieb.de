77b2cc4d49de3a610c4bb1f02bc233960f2cb824
Jan <mail@janchristlieb.de>
2018-03-21 18:16:08 +0100

    new intro image

---
+++ resources/views/about.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.master')
+
+
+@section('intro')
+    <div class="section_welcome text-center m-4">
+        <img class="img-intro" src="images/Jan-Christlieb.jpg">
+        <h3 class="mt-4">Jan Christlieb</h3>
+        <hr class="hr-intro">
+        <p>Web developer from Berlin</p>
+    </div>
+@endsection
+
+@section('content')
+    <div class="container section_welcome">
+        <div class="row">
+            <div class="col-12">
+                <div class="jumbotron mt-4 mb-4">
+                    <h1 class="display-4 mb-4">Hello.</h1>
+                    <p class="lead">Welcome to my little corner of the web. My name is Jan. I build web sites and
+                        web
+                        applications for clients and for fun.</p>
+                    <p class="lead">Feel free to look around. You may review my projects or read about why I do what
+                        I
+                        do.</p>
+                    <p class="lead">Looking for someone to collaborate with? Let's chat: mail@janchristlieb.de</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
+
+
+
