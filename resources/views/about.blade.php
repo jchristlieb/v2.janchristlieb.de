6d556cb4b1b483129aee29aa8ba60af4e32f1c8a
Jan <mail@janchristlieb.de>
2018-03-21 18:17:24 +0100

    added about view

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,11 +2,11 @@
 
 
 @section('intro')
-    <div class="section_welcome text-center m-4">
-        <img class="img-intro" src="images/Jan-Christlieb.jpg">
-        <h3 class="mt-4">Jan Christlieb</h3>
-        <hr class="hr-intro">
-        <p>Web developer from Berlin</p>
+    <div class="row">
+        <div class="intro">
+            <h1 class="display-4 mb-4">My story</h1>
+            <p class="lead">From IT Management to entrepreneurship to web development</p>
+        </div>
     </div>
 @endsection
 
@@ -14,16 +14,28 @@
     <div class="container section_welcome">
         <div class="row">
             <div class="col-12">
-                <div class="jumbotron mt-4 mb-4">
-                    <h1 class="display-4 mb-4">Hello.</h1>
-                    <p class="lead">Welcome to my little corner of the web. My name is Jan. I build web sites and
-                        web
-                        applications for clients and for fun.</p>
-                    <p class="lead">Feel free to look around. You may review my projects or read about why I do what
-                        I
-                        do.</p>
-                    <p class="lead">Looking for someone to collaborate with? Let's chat: mail@janchristlieb.de</p>
-                </div>
+                <p>Hi, my name is Jan. I life in Berlin and work as a freelance web developer. However, this is not what
+                    I have done my entire career.</p>
+                <p>Indeed my focus throughout my studies were on management and marketing. I earned master degrees from
+                    the University of Twente in Business Administration and the TU Berlin in Innovation Management.</p>
+                <p>Consequently, I started my career in an IT management position as a product owner for a credit
+                    reporting agency. It turned out that product development from a business perspective was not as
+                    satisfying as I hoped it to be.</p>
+                <p>I resigned after two years. At this time my next task was not set but my vision was clear. Doing
+                    something I am truly passionate about.</p>
+                <p>I love bicycles. I use them as a mean of transportation, as a sport utility, and I like to handpick
+                    the components and assemble them myself. Passion detected.</p>
+                <p>Radweiser was the attempt to combine my passion and my profession. The idea was to help people find a
+                    bike that suits their needs and wants. Therefore I started to develop a bike consultancy app.</p>
+                <p>While working on the market maturity of my project, I learned two lessons: first, technical skills
+                    are essential for entrepreneurial survival, and second, my strong limitation of exactly those skills
+                    in need.</p>
+                <p>I paused Radweiser due to my (former) disability of transforming my idea of a profound bike
+                    consultancy app into a competitive product. Nevertheless, I developed a distinct joy and fascination
+                    for code and grasped the basics of HTML, CSS, Javascript and PHP.</p>
+                <p>Those acquired coding skills helped me to land opportunities as freelance developer. My goal is to
+                    deliberately practice and develop my coding capabilities. It is a challenging and satisfying path
+                    that I look forward walking on.</p>
             </div>
         </div>
     </div>
