4af3588e9962d17e0dbb94263e2a0586b347e7af
Jan <mail@janchristlieb.de>
2018-03-21 18:17:05 +0100

    added legal view

--- resources/views/legal.blade.php
+++ resources/views/legal.blade.php
@@ -4,38 +4,28 @@
 @section('intro')
     <div class="row">
         <div class="intro">
-            <h1 class="display-4 mb-4">My story</h1>
-            <p class="lead">From IT Management to entrepreneurship to web development</p>
+            <h1 class="display-4">Legal notice</h1>
         </div>
     </div>
 @endsection
 
 @section('content')
-    <div class="container section_welcome">
+    <div class="container">
         <div class="row">
             <div class="col-12">
-                <p>Hi, my name is Jan. I life in Berlin and work as a freelance web developer. However, this is not what
-                    I have done my entire career.</p>
-                <p>Indeed my focus throughout my studies were on management and marketing. I earned master degrees from
-                    the University of Twente in Business Administration and the TU Berlin in Innovation Management.</p>
-                <p>Consequently, I started my career in an IT management position as a product owner for a credit
-                    reporting agency. It turned out that product development from a business perspective was not as
-                    satisfying as I hoped it to be.</p>
-                <p>I resigned after two years. At this time my next task was not set but my vision was clear. Doing
-                    something I am truly passionate about.</p>
-                <p>I love bicycles. I use them as a mean of transportation, as a sport utility, and I like to handpick
-                    the components and assemble them myself. Passion detected.</p>
-                <p>Radweiser was the attempt to combine my passion and my profession. The idea was to help people find a
-                    bike that suits their needs and wants. Therefore I started to develop a bike consultancy app.</p>
-                <p>While working on the market maturity of my project, I learned two lessons: first, technical skills
-                    are essential for entrepreneurial survival, and second, my strong limitation of exactly those skills
-                    in need.</p>
-                <p>I paused Radweiser due to my (former) disability of transforming my idea of a profound bike
-                    consultancy app into a competitive product. Nevertheless, I developed a distinct joy and fascination
-                    for code and grasped the basics of HTML, CSS, Javascript and PHP.</p>
-                <p>Those acquired coding skills helped me to land opportunities as freelance developer. My goal is to
-                    deliberately practice and develop my coding capabilities. It is a challenging and satisfying path
-                    that I look forward walking on.</p>
+                <h5 class="mb-4">Liability and information according to §5 TMG</h5>
+                <p>Jan Christlieb<br>
+                    Schwartzkopffstraße 4<br>
+                    10115 Berlin<br></p>
+                <p>Mobil: 0176 / 99983308<br>
+                    Email: mail [at] janchristlieb [dot] de<br>
+                    USt-IdNr.: DE305359246</p>
+            </div>
+            <div class="col-12">
+                <h5 class="mt-4 mb-4">Responsible for content according to §55 paragraph 2 RStV</h5>
+                <p>Jan Christlieb<br>
+                    Schwartzkopffstraße 4<br>
+                    10115 Berlin<br></p>
             </div>
         </div>
     </div>
